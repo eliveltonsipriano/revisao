3846aee1013ccf9be0f20c2785d00ac1076d6396
elivelton_sipriano <elivelton_sipriano@10.1.49.178>
2013-07-31 14:59:59 -0300

    adicionado a classe pessoa.

---
+++ src/br/senai/sc/model/negocio/PessoaFisica.java
@@ -0,0 +1,55 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.senai.sc.model.negocio;
+
+/**
+ * Classe que representa as entidasdes das pessoas fisicas do software
+ *
+ * @author elivelton_sipriano
+ * @version 1.0 31/07/13
+ */
+public class PessoaFisica extends Pessoa {
+
+    private String cpf;
+    private String nome;
+    private String dataNascimento;
+    private String rg;
+
+    public PessoaFisica() {
+    }
+    
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDataNascimento() {
+        return dataNascimento;
+    }
+
+    public void setDataNascimento(String dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+
+    public String getRg() {
+        return rg;
+    }
+
+    public void setRg(String rg) {
+        this.rg = rg;
+    }
+}
