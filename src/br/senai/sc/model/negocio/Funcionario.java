8bf25f34544e5f510c023b90a64f88e13617e6f7
elivelton_sipriano <elivelton_sipriano@10.1.49.178>
2013-07-31 15:34:39 -0300

    adicionado a classe Funcionario.

---
+++ src/br/senai/sc/model/negocio/Funcionario.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.senai.sc.model.negocio;
+
+/**
+ *classe que representa os funcionarios do software
+ * @version 1.0 31/07/13
+ * @author elivelton_sipriano
+ */
+public class Funcionario extends PessoaFisica{
+    
+}
