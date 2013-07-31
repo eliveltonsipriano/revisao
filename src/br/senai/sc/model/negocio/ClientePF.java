1e4424a5cd863c29ed4b2c0471de6c35ddcfd35a
elivelton_sipriano <elivelton_sipriano@10.1.49.178>
2013-07-31 15:45:29 -0300

    adicionado a classeClientePF.

---
+++ src/br/senai/sc/model/negocio/ClientePF.java
@@ -0,0 +1,64 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.senai.sc.model.negocio;
+
+/**
+ * classe que representa os clientes fisicos do software
+ * @version 1.0 31/07/13
+ * @author elivelton_sipriano
+ */
+public class ClientePF extends PessoaFisica {
+    private Double nrConta;
+    private double LimiteCompra;
+    private char categoria;
+    private int qtVezesComprou;
+    private double v1TotalGasto;
+
+    public ClientePF() {
+    }
+    
+
+    public Double getNrConta() {
+        return nrConta;
+    }
+
+    public void setNrConta(Double nrConta) {
+        this.nrConta = nrConta;
+    }
+
+    public double getLimiteCompra() {
+        return LimiteCompra;
+    }
+
+    public void setLimiteCompra(double LimiteCompra) {
+        this.LimiteCompra = LimiteCompra;
+    }
+
+    public char getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(char categoria) {
+        this.categoria = categoria;
+    }
+
+    public int getQtVezesComprou() {
+        return qtVezesComprou;
+    }
+
+    public void setQtVezesComprou(int qtVezesComprou) {
+        this.qtVezesComprou = qtVezesComprou;
+    }
+
+    public double getV1TotalGasto() {
+        return v1TotalGasto;
+    }
+
+    public void setV1TotalGasto(double v1TotalGasto) {
+        this.v1TotalGasto = v1TotalGasto;
+    }
+    
+    
+}
