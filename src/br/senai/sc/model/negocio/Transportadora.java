3e50854cd3225673485d93875c5e3b58210b2e21
elivelton_sipriano <elivelton_sipriano@10.1.49.178>
2013-07-31 16:28:57 -0300

    adicionando a classe Transportadora

--- src/br/senai/sc/model/negocio/Transportadora.java
+++ src/br/senai/sc/model/negocio/Transportadora.java
@@ -1,13 +1,13 @@
-
 package br.senai.sc.model.negocio;
 
 /**
- *classe que representa as transportadoras do software
+ * classe que representa as transportadoras do software
  *
  * @version 1.0 31/07/13
  * @author elivelton_sipriano
  */
 public class Transportadora extends PessoaJuridica {
+
     private String tipoTransporte;
     private double precoFrete;
 
@@ -29,7 +29,4 @@ public class Transportadora extends PessoaJuridica {
     public void setPrecoFrete(double precoFrete) {
         this.precoFrete = precoFrete;
     }
-    
-    
-    
 }
