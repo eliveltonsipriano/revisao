34147cce870a40f191c3f6eb70e8fa8234977714
elivelton_sipriano <elivelton_sipriano@10.1.49.178>
2013-07-31 14:48:41 -0300

    adicionado a classe pessoa.

---
+++ src/br/senai/sc/model/negocio/Pessoa.java
@@ -0,0 +1,54 @@
+
+package br.senai.sc.model.negocio;
+
+/**
+ * Essa classe representa pessoas do software
+ * @version 1.0 31/07/13
+ * @author elivelton_sipriano
+ */
+public class Pessoa {
+    
+  private  int id;
+   private String endereco;
+   private String telefone;
+   private String dataCadastro;
+
+    public Pessoa() {
+    }
+   
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getDataCadastro() {
+        return dataCadastro;
+    }
+
+    public void setDataCadastro(String dataCadastro) {
+        this.dataCadastro = dataCadastro;
+    }
+   
+   
+   
+}
