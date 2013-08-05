eed98578675a3497d1e09bbcc6541d48d41206f8
elivelton_sipriano <elivelton_sipriano@10.1.49.178>
2013-08-05 16:52:39 -0300

    adicionei varios metodos na janela principal

--- src/br/senai/sc/view/JanelaPrincipal.java
+++ src/br/senai/sc/view/JanelaPrincipal.java
@@ -1,20 +1,38 @@
 package br.senai.sc.view;
 
+import br.senai.sc.model.negocio.Funcionario;
+import br.senai.sc.percistencia.FuncionarioDAO;
+import java.util.ArrayList;
+import java.util.List;
 import javax.swing.JOptionPane;
 
 public class JanelaPrincipal {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) {    
+        List<Funcionario>  funcionarios = new ArrayList<>();
+               
+//Vetor dinâmico para armazenar os funcionarios cadastrados.
+
+        
+        
+        mostraMenu(funcionarios);
+
+
+
+
     }
 
-    public void mostraMenu() {
+    public static void mostraMenu( List<Funcionario> funcionarios ) {
+        JanelaFuncionario jf = new JanelaFuncionario();
+
         int opcao = 0;
         do {
-            opcao = Integer.parseInt(JOptionPane.showInputDialog("Informe  opção:" + "\n1 - Funcionário" + "\n1 - Cliente" + "\n1 - fornecedor" + "\n1 - Transportadora" + "\n1 - Sair"));
+            opcao = Integer.parseInt(JOptionPane.showInputDialog("Informe  opção:" + "\n1 - Funcionário" + "\n2 - Cliente" + "\n3 - fornecedor" + "\n4 - Transportadora" + "\n5 - Sair"));
 
 
             switch (opcao) {
                 case 1:
+                    jf.menuFuncionario(funcionarios);
                     break;
                 case 2:
                     break;
